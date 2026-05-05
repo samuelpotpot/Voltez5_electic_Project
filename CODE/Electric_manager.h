f7d3b471ef90b2f7419780506738c76f2b4a0ee8
samuelpotpot <samuel063007@gmail.com>
2026-05-05 21:17:02 +0800

    Update Electric_manager.h

--- CODE/Electric_manager.h
+++ CODE/Electric_manager.h
@@ -1,1 +1,10 @@
-header
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include <string>
+using namespace std;
+
+void inputData(string names[], float watts[], float hours[], int size);
+void computeUsage(float watts[], float hours[], float usage[], int size, float &total);
+
+#endif
