c2001b727f9329dadb5ea7b1d53a4fb991dc048c
samuelpotpot <samuel063007@gmail.com>
2026-05-13 13:23:51 +0800

    Update Electric.h

--- CODE/Electric.h
+++ CODE/Electric.h
@@ -1,10 +1,15 @@
-#ifndef DISPLAY_H
-#define DISPLAY_H
-
+#include <iostream>
+#include <fstream>
 #include <string>
+#include <vector>
+#include <iomanip>
+
 using namespace std;
 
-void displayReport(string names[], float usage[], float hours[], int size, float total, float userSavings);
-void showAppliances(string names[], int size);
+struct Appliance {
+    string name;
+    double watts;
+    double hours;
+};
 
-#endif
+string currentUser = "";
